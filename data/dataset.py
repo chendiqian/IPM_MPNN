2d31bde88256382581a1e2bedf10ff9b6e6f2598
Chendi Qian <1049653012@qq.com>
2023-07-04 12:04:17 +0200

    pack more ip in one pkl

--- data/dataset.py
+++ data/dataset.py
@@ -41,7 +41,7 @@ class SetCoverDataset(InMemoryDataset):
 
     @property
     def raw_file_names(self) -> List[str]:
-        return [f'instance_{i}.pkl.gz' for i in range(1000)]
+        return ['instance_0.pkl.gz']   # there should be at least one pkg
 
     @property
     def processed_dir(self) -> str:
@@ -52,77 +52,80 @@ class SetCoverDataset(InMemoryDataset):
         return ['data.pt']
 
     def process(self):
-        num_instance = len(os.listdir(self.raw_dir))
+        num_instance_pkg = len([n for n in os.listdir(self.raw_dir) if n.endswith('pkl.gz')])
 
         data_list = []
-        for i in range(num_instance):
+        for i in range(num_instance_pkg):
             # load instance
-            print(f"processing {i}th instance")
+            print(f"processing {i}th package, {num_instance_pkg} in total")
             with gzip.open(os.path.join(self.raw_dir, f"instance_{i}.pkl.gz"), "rb") as file:
-                (A, b, c) = pickle.load(file)
+                ip_pkgs = pickle.load(file)
+
+            for ip_idx in range(len(ip_pkgs)):
+                (A, b, c) = ip_pkgs[ip_idx]
                 c = c / c.max()  # does not change the result
 
-            # solve the LP
-            # sol = ipm_overleaf(c.numpy(), None, None, A.numpy(), b.numpy(), None, max_iter=1000, lin_solver='scipy_cg')
+                for _ in range(self.rand_starts):
+                    # solve the LP
+                    # sol = ipm_overleaf(c.numpy(), None, None, A.numpy(), b.numpy(), None, max_iter=1000, lin_solver='scipy_cg')
 
-            for _ in range(self.rand_starts):
-                sol = linprog(c.numpy(),
-                              A_ub=None,
-                              b_ub=None,
-                              A_eq=A.numpy(), b_eq=b.numpy(), bounds=None,
-                              method='interior-point', callback=lambda res: res.x)
+                    sol = linprog(c.numpy(),
+                                  A_ub=None,
+                                  b_ub=None,
+                                  A_eq=A.numpy(), b_eq=b.numpy(), bounds=None,
+                                  method='interior-point', callback=lambda res: res.x)
 
-                # organize results
-                # x, l, s = zip(*sol['xs'])
-                # x = np.stack(x, axis=1)  # primal
-                # l = np.stack(l, axis=1)  # dual
-                # s = np.stack(s, axis=1)  # slack
+                    # organize results
+                    # x, l, s = zip(*sol['xs'])
+                    # x = np.stack(x, axis=1)  # primal
+                    # l = np.stack(l, axis=1)  # dual
+                    # s = np.stack(s, axis=1)  # slack
 
-                # x = np.stack([i['x'] for i in sol.intermediate], axis=1)
-                x = np.stack(sol.intermediate, axis=1)
+                    # x = np.stack([i['x'] for i in sol.intermediate], axis=1)
+                    x = np.stack(sol.intermediate, axis=1)
 
-                # l = np.stack([i['con'] for i in sol.intermediate], axis=1)
+                    # l = np.stack([i['con'] for i in sol.intermediate], axis=1)
 
-                gt_primals = torch.from_numpy(x).to(torch.float)
-                # gt_duals = torch.from_numpy(l).to(torch.float)
-                # gt_slacks = torch.from_numpy(s).to(torch.float)
+                    gt_primals = torch.from_numpy(x).to(torch.float)
+                    # gt_duals = torch.from_numpy(l).to(torch.float)
+                    # gt_slacks = torch.from_numpy(s).to(torch.float)
 
-                data = HeteroData(
-                    cons={'x': torch.cat([A.mean(1, keepdims=True),
-                                          A.std(1, keepdims=True)], dim=1)},
-                    vals={'x': torch.cat([A.mean(0, keepdims=True),
-                                          A.std(0, keepdims=True)], dim=0).T},
-                    obj={'x': torch.cat([c.mean(0, keepdims=True),
-                                         c.std(0, keepdims=True)], dim=0)[None]},
+                    data = HeteroData(
+                        cons={'x': torch.cat([A.mean(1, keepdims=True),
+                                              A.std(1, keepdims=True)], dim=1)},
+                        vals={'x': torch.cat([A.mean(0, keepdims=True),
+                                              A.std(0, keepdims=True)], dim=0).T},
+                        obj={'x': torch.cat([c.mean(0, keepdims=True),
+                                             c.std(0, keepdims=True)], dim=0)[None]},
 
-                    cons__to__vals={'edge_index': torch.vstack(torch.where(A)),
-                                    'edge_attr': A[torch.where(A)][:, None]},
-                    vals__to__cons={'edge_index': torch.vstack(torch.where(A.T)),
-                                    'edge_attr': A.T[torch.where(A.T)][:, None]},
-                    vals__to__obj={'edge_index': torch.vstack([torch.arange(A.shape[1]),
-                                                               torch.zeros(A.shape[1], dtype=torch.long)]),
-                                   'edge_attr': c[:, None]},
-                    obj__to__vals={'edge_index': torch.vstack([torch.zeros(A.shape[1], dtype=torch.long),
-                                                               torch.arange(A.shape[1])]),
-                                   'edge_attr': c[:, None]},
-                    cons__to__obj={'edge_index': torch.vstack([torch.arange(A.shape[0]),
-                                                               torch.zeros(A.shape[0], dtype=torch.long)]),
-                                   'edge_attr': b[:, None]},
-                    obj__to__cons={'edge_index': torch.vstack([torch.zeros(A.shape[0], dtype=torch.long),
-                                                               torch.arange(A.shape[0])]),
-                                   'edge_attr': b[:, None]},
-                    gt_primals=gt_primals,
-                    # gt_duals=gt_duals,
-                    # gt_slacks=gt_slacks,
-                    obj_value=torch.tensor(sol['fun'].astype(np.float32)),
-                    obj_const=c)
+                        cons__to__vals={'edge_index': torch.vstack(torch.where(A)),
+                                        'edge_attr': A[torch.where(A)][:, None]},
+                        vals__to__cons={'edge_index': torch.vstack(torch.where(A.T)),
+                                        'edge_attr': A.T[torch.where(A.T)][:, None]},
+                        vals__to__obj={'edge_index': torch.vstack([torch.arange(A.shape[1]),
+                                                                   torch.zeros(A.shape[1], dtype=torch.long)]),
+                                       'edge_attr': c[:, None]},
+                        obj__to__vals={'edge_index': torch.vstack([torch.zeros(A.shape[1], dtype=torch.long),
+                                                                   torch.arange(A.shape[1])]),
+                                       'edge_attr': c[:, None]},
+                        cons__to__obj={'edge_index': torch.vstack([torch.arange(A.shape[0]),
+                                                                   torch.zeros(A.shape[0], dtype=torch.long)]),
+                                       'edge_attr': b[:, None]},
+                        obj__to__cons={'edge_index': torch.vstack([torch.zeros(A.shape[0], dtype=torch.long),
+                                                                   torch.arange(A.shape[0])]),
+                                       'edge_attr': b[:, None]},
+                        gt_primals=gt_primals,
+                        # gt_duals=gt_duals,
+                        # gt_slacks=gt_slacks,
+                        obj_value=torch.tensor(sol['fun'].astype(np.float32)),
+                        obj_const=c)
 
-                if self.pre_filter is not None:
-                    raise NotImplementedError
+                    if self.pre_filter is not None:
+                        raise NotImplementedError
 
-                if self.pre_transform is not None:
-                    data = self.pre_transform(data)
+                    if self.pre_transform is not None:
+                        data = self.pre_transform(data)
 
-                data_list.append(data)
+                    data_list.append(data)
 
         torch.save(self.collate(data_list), osp.join(self.processed_dir, 'data.pt'))
